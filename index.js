4cca3531a2d6b0404a6a1e040d413ece5527b976
seoyeon416 <150769053+seoyeon416@users.noreply.github.com>
2023-11-29 16:26:00 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Export all hanspell implementations.
- */
-
-const spellCheckByDAUM = require('./daum-spell-check');
-const spellCheckByPNU = require('./pnu-spell-check');
-
-module.exports = { spellCheckByDAUM, spellCheckByPNU };
